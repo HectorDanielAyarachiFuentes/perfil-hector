3abe54178f9c465304d8a6a51c81af2e62ff7a5f
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2024-02-01 20:13:37 -0300

    // Para descargar en formato PDF     function downloadPDF() {         // Load print-optimized CSS         var link = document.createElement('link');         link.rel = 'stylesheet';         link.type = 'text/css';         link.href = 'print-style.css'; // Asegúrate de que print-style.css esté presente         link.media = 'all';
            // Clone the body and apply the print stylesheet
            var body = document.body.cloneNode(true);
            body.appendChild(link);
    
            var opt = {
                margin:       [15, 15, 15, 15],
                filename:     'CV-Hector Daniel-Ayarachi Fuentes.pdf',
                image:        { type: 'jpeg', quality: 0.98 },
                html2canvas:  { scale: 2 },
                jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
            };
    
            html2pdf().from(body).set(opt).save();
    
            return false; // Prevenir el comportamiento predeterminado del enlace
        }

--- script.js
+++ script.js
@@ -14,3 +14,31 @@
           // Redirigir a la URL final
           window.open(finalURL, '_blank');
         });
+
+
+
+     // Para descargar en formato PDF
+    function downloadPDF() {
+      // Load print-optimized CSS
+      var link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = 'text/css';
+      link.href = 'print-style.css'; // Asegúrate de que print-style.css esté presente
+      link.media = 'all';
+
+      // Clone the body and apply the print stylesheet
+      var body = document.body.cloneNode(true);
+      body.appendChild(link);
+      
+      var opt = {
+          margin:       [15, 15, 15, 15],
+          filename:     'CV-Hector Daniel-Ayarachi Fuentes.pdf',
+          image:        { type: 'jpeg', quality: 0.98 },
+          html2canvas:  { scale: 2 },
+          jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+      };
+
+      html2pdf().from(body).set(opt).save();
+      
+      return false; // Prevenir el comportamiento predeterminado del enlace
+  }
