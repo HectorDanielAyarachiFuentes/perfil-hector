50b68ff1ad6c99322ecb9da1fb992e3c099f4df0
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2024-02-01 20:54:02 -0300

    ^$\n

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-                  // funcion para que facebook se abra tanto en celulares como escritorio-inicio
+// funcion para que facebook se abra tanto en celulares como escritorio-inicio
 
-        
-        document.getElementById('facebookLink').addEventListener('click', function(event) {
-          // Evitar el comportamiento predeterminado del enlace
-          event.preventDefault();
-        
-          // URL del perfil de Facebook
-          var facebookProfileURL = 'https://www.facebook.com/HECTORDANIELAYARACHI';
-        
-          // Verificar si estamos en un dispositivo móvil
-          var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-        
-          // Construir el enlace adecuado según el dispositivo
-          var finalURL = isMobile ? 'fb://profile?id=HECTORDANIELAYARACHI' : facebookProfileURL;
-        
-          // Redirigir a la URL final
-          window.open(finalURL, '_blank');
-        });
-                  // funcion para que facebook se abra tanto en celulares como escritorio-fin
+document.getElementById('facebookLink').addEventListener('click', function (event) {
+  // Evitar el comportamiento predeterminado del enlace
+  event.preventDefault();
 
-                  // funcion para que se descargue todo el contenido del html-incio
+  // URL del perfil de Facebook
+  var facebookProfileURL = 'https://www.facebook.com/HECTORDANIELAYARACHI';
 
-        function downloadPDF() {
-          // Retraso para cargar contenido dinámico
-          setTimeout(() => {
-            const element = window.print(); // Captura toda la página
-            html2pdf(element, {
-              margin: 10,
-              filename: 'mi_curriculum.pdf',
-              image: { type: 'jpeg', quality: 0.98 },
-              html2canvas: { scale: 2 },
-              jsPDF: { unit: 'mm', format: 'a3', orientation: 'portrait' }
-            });
-          }, 1000);
-        }
-        
-                  // funcion para que se descargue todo el contenido del html-fin
+  // Verificar si estamos en un dispositivo móvil
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+  // Construir el enlace adecuado según el dispositivo
+  var finalURL = isMobile ? 'fb://profile?id=HECTORDANIELAYARACHI' : facebookProfileURL;
 
+  // Redirigir a la URL final
+  window.open(finalURL, '_blank');
+});
+// funcion para que facebook se abra tanto en celulares como escritorio-fin
+// funcion para que se descargue todo el contenido del html-incio
+function downloadPDF() {
+  // Retraso para cargar contenido dinámico
+  setTimeout(() => {
+    const element = window.print(); // Captura toda la página
+    html2pdf(element, {
+      margin: 10,
+      filename: 'mi_curriculum.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a3', orientation: 'portrait' }
+    });
+  }, 1000);
+}
+
+// funcion para que se descargue todo el contenido del html-fin
